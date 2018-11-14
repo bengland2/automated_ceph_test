0f156f8cf8a461b7d50c986426cd1fadd6f092cb
Ben England <bengland@redhat.com>
2018-11-14 08:52:17 -0500

    allow quick teardown without waiting teardown_delay min
    also tell user what to do and what to expect
    force cephfs unmounts if dangling mount left over

--- scripts/jobs/5-Teardown_Linode_Ceph_Cluster.sh
+++ scripts/jobs/5-Teardown_Linode_Ceph_Cluster.sh
@@ -10,14 +10,42 @@
 # Jenkins will stop the pipeline immediately,
 # this makes troubleshooting a pipeline much easier for the user
 
-(( teardown_sec = $teardown_delay_minutes * 60 ))
-echo "waiting $teardown_sec seconds before tear-down"
-sleep $teardown_sec
+echo "to suspend teardown indefinitely, "
+echo "touch /tmp/suspend-teardown before teardown delay expires"
+echo "to cause immediate teardown, touch /tmp/teardown-now"
 
-while [ -f /tmp/suspend_teardown ] ; do
-	echo "waiting for /tmp/suspend_teardown file to be removed..."
-	sleep 60
+(( teardown_sec = $teardown_delay_minutes * 60 ))
+while [ $teardown_sec -gt 0 ] ; do
+    if [ -f /tmp/teardown-now ] ; then
+        echo "saw /tmp/teardown-now, starting tear-down now"
+        rm -fv /tmp/teardown_now
+        break
+    fi
+    sleep 60
+    (( teardown_sec = $teardown_sec - 60 ))
+    echo "waiting $teardown_sec seconds before tear-down"
 done
+
+# this feature enables script debugging for pipelines
+# you can't debug scripts once linodes are destroyed
+
+while [ -f /tmp/suspend-teardown ] ; do
+    echo "waiting for /tmp/suspend-teardown file to be removed..."
+    sleep 60
+done
+
+# first tear down Ceph on this agent 
+# so we don't have dangling mountpoints, etc
+
+umount -a -ff -t ceph -v
+yum remove -y ceph-common ceph-fuse librados2 ceph-ansible
+leftovers=`rpm -qa | awk '/ceph/||/librados/||/librbd/' | wc -l` 
+if [ $leftovers -gt 0 ] ; then
+    echo 'not all Ceph RPMS removed!'
+    rpm -qa | awk '/ceph/||/librados/||/librbd/'
+fi
+
+# now tear down the linode cluster
 
 cd $HOME/ceph-linode/
 virtualenv-2 linode-env && source linode-env/bin/activate && pip install linode-python
